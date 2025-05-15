dc100996a035e4eb1a272e6848faf8ab17593534
devjindal135 <devjindal135@gmail.com>
2025-05-15 16:30:23 +0530

    MemeBot

---
+++ README.md
@@ -0,0 +1,36 @@
+# Discord Bot
+
+A Discord bot built with Python.
+
+## Features
+
+- [List your bot's features here]
+
+## Installation
+
+1. Clone the repository
+2. Install dependencies:
+   ```
+   pip install -r requirements.txt
+   ```
+3. Configure your environment variables (see Configuration section)
+4. Run the bot:
+   ```
+   python main.py
+   ```
+
+## Configuration
+
+Create a `.env` file in the root directory with the following variables:
+
+```
+DISCORD_TOKEN=your_discord_token
+```
+
+## Commands
+
+- [List your bot commands here]
+
+## License
+
+[Your chosen license] 
---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+import requests
+import json
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, messages):
+        if messages.author == self.user:
+            return
+        
+        if messages.content.startswith('$meme'):
+            await messages.channel.send(get_meme())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(config['API_KEY'])
